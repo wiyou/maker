f8c227fdb3f392e83f021aaf82824d19ae28a8ee
Bogaardt <walter.c.bogaardt@disney.com>
2020-04-07 08:27:04 -0700

    added slider for eye fixed apk

--- hocuspocus/audrino/animatronic-eye-ble/animatronic-eye-ble.ino
+++ hocuspocus/audrino/animatronic-eye-ble/animatronic-eye-ble.ino
@@ -11,13 +11,13 @@
 Servo eyelid;
 Servo eyeball;
 SoftwareSerial mySerial(6,7); // RX | TX
-int BluetoothData; // the data given from Computer
+String BluetoothData = "";
 int posLid = 0; 
-int posBall = 0;
+int posBall = 60;
 int maxLeft = 150;
 int maxRight = 30;
-int maxClose = 180;
-int maxOpen = 30;
+int maxClose = 174;
+int maxOpen = 110;
 int ledpin=13;
 int EYELID=3;
 int EYEBALL=4;
@@ -30,67 +30,62 @@ void setup() {
     mySerial.begin(9600);  
 
     pinMode(ledpin,OUTPUT);
+    //attach servo to pin.
+    eyelid.attach(EYELID);
+    eyeball.attach(EYEBALL);
     
-  eyelid.attach(EYELID);
-  eyeball.attach(EYEBALL);
-  // put your setup code here, to run once:
-  //pinMode(LED_BUILTIN, OUTPUT);
-  eyeball.write(maxRight);
-  eyelid.write(maxClose);
+    eyeball.write(posBall);
+    eyelid.write(maxClose);
 }
 
 void loop() {
-   if (mySerial.available())
-    {
-        BluetoothData = mySerial.read();
-        Serial.write(BluetoothData);
-        if(BluetoothData=='1') { // if number 1 pressed ….blink
-          blinkEye(5);
+    while (mySerial.available()) {                    // loop through while data is available
+      char character = mySerial.read(); // Receive a single character from the software serial port
+      BluetoothData.concat(character); // Add the received character to the receive buffer
+      if (character == '\n')
+      {
+          Serial.print("Received: ");
+          Serial.println(BluetoothData);
+          int val = BluetoothData.toInt();
+         if(val==1) { // if number 1 pressed ….blink
+          blinkEye(3);
+          openEyeNormal(2);
           Serial.println("Blink");
-       }
-        if(BluetoothData=='2') { // if number 2 pressed ….Wakeup
+         }
+        if( val==2) { // if number 2 pressed ….Wakeup
           Serial.println("Wake Up");
           Serial.print(posLid); Serial.println(" Lid position");
-          wakeUp();
+          book();
         }
-         if(BluetoothData=='3') { // if number 3 pressed ….Sleep
+         if( val==3) { // if number 3 pressed ….Sleep
           Serial.println("Sleep ");
-          closeEye(10);
+          sleepEye();
         }
-          if(BluetoothData=='4') { // if number 3 pressed ….Sleep
+          if( val==4) { // if number 3 pressed ….Sleep
           Serial.println("Amok ");
-          closeEye(10);
+          wakeup();
         }
-          if(BluetoothData=='5') { // if number 3 pressed ….Sleep
+          if( val==5) { // if number 3 pressed ….Sleep
           Serial.println("Open ");
-          openEye(4);
+          openEye(3);
+          openEyeNormal(2);
         }
-    }
-     
- // delay(100);// prepare for next data …
- /*openEye(5);
- Serial.println("open:");
- delay(1000);
- closeEye(5);
- Serial.println("close:");
- delay(2000);
- 
- blinkEye(4);
- delay(3000);*/
- /*openEye(5);
-  moveEyeBalLeft(maxLeft,10);
-  delay(3000);  
-  moveEyeBallRight(0,10);
-  closeEye(5);
- // delay(4000);
-  //moveEyeBallCenter(5);
-  //blinkEye(5);
-  // blinkEye(4);
-  delay(1000);
- // wakeUp();
-  delay(3000);*/
- 
-  
+        if( val >= 30) {
+          Serial.print("Eye move ");
+          Serial.println(val);
+          if(posBall >= val) {
+            moveEyeBallRight(val,5);
+          } else {
+            moveEyeBalLeft(val,5);
+          }
+       }
+          BluetoothData = "";
+      }
+        //            }
+                                     
+
+       /**/
+    } 
 }
 
 void moveEyeBallCenter(int spd) {
@@ -98,8 +93,7 @@ void moveEyeBallCenter(int spd) {
     for (; posBall <= 90; posBall += 1) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
       eyeball.write(posBall);              // tell servo to go to position in variable 'pos'
-      delay(spd);                       // waits 15ms for the servo to reach the position
-    }
+      delay(spd);                       // waits 15ms for the servo to reach the positio 
    }
    if(posBall > 90) {
     for (; posBall >= 90; posBall -= 1) { // goes from 180 degrees to 0 degrees
@@ -108,6 +102,7 @@ void moveEyeBallCenter(int spd) {
     } 
    }
    posBall = 90;
+   }
 }
 
 void moveEyeBalLeft(int finalPos, int spd)
@@ -126,29 +121,71 @@ void moveEyeBallRight(int finalPos, int spd){
    }
 }
 
-void wakeUp() {
-  if(posLid=maxOpen) {
-    closeEye(4);
+void book() {
+  if(posLid<=maxOpen-20) {
+    shutEye(4);
     moveEyeBallCenter(4);
-    for (; posLid >= 70; posLid -= 1) { // goes from 0 degrees to 180 degrees
+  }
+    for (; posLid >= 130; posLid -= 1) { // goes from 0 degrees to 180 degrees
       eyelid.write(posLid);              // tell servo to go to position in variable 'pos'
       delay(3);
     }
-    closeEye(10);
-    for (; posLid >= 70; posLid -= 1) { // goes from 0 degrees to 180 degrees
+    shutEye(10);
+    for (; posLid >= 130; posLid -= 1) { // goes from 0 degrees to 180 degrees
       eyelid.write(posLid);              // tell servo to go to position in variable 'pos'
       delay(3);
     }
-    openEye(4);
+    openEyeNormal(4);
     for(int cnt=0; cnt<5; cnt++) {
-      moveEyeBalLeft(maxLeft,5);
-      moveEyeBallRight(maxRight,5);
+      moveEyeBalLeft(maxLeft,4);
+      delay(1000);
+      moveEyeBallRight(maxRight,4);
     }
-    moveEyeBallCenter(4);
-  }        
+    moveEyeBallCenter(4);    
+}    
+
+  void wakeup() {
+    if(posLid<=maxOpen-30) {
+      shutEye(4);
+      moveEyeBallCenter(4);
+    }
+    for (; posLid >= 145; posLid -= 1) { // goes from 0 degrees to 180 degrees
+      eyelid.write(posLid);              // tell servo to go to position in variable 'pos'
+      delay(5);
+    }
+    for(int cnt=0; cnt<3; cnt++) {
+      moveEyeBalLeft(maxLeft-10,10);
+      delay(500);
+      moveEyeBallRight(maxRight+10,10);
+      delay(500);
+    }
+    shutEye(10);
+    for (; posLid >= 130; posLid -= 1) { // goes from 0 degrees to 180 degrees
+      eyelid.write(posLid);              // tell servo to go to position in variable 'pos'
+      delay(3);
+    }
+    openEyeNormal(4);
+    for(int cnt=0; cnt<2; cnt++) {
+      moveEyeBalLeft(maxLeft,4);
+      delay(1000);
+      moveEyeBallRight(maxRight,4);
+    }
+    moveEyeBallCenter(4);       
 }
 
-void closeEye(int spd) {
+void sleepEye(void) {
+  moveEyeBallCenter(4); 
+  blinkEye(3); 
+  for (; posLid <= 150; posLid += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+     eyelid.write(posLid);              // tell servo to go to position in variable 'pos'
+     delay(14);                       // waits 15ms for the servo to reach the position
+   }
+   openEyeNormal(3);
+   shutEye(10);
+}
+
+void shutEye(int spd) {
      for (; posLid <= maxClose; posLid += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
      eyelid.write(posLid);              // tell servo to go to position in variable 'pos'
@@ -156,16 +193,34 @@ void closeEye(int spd) {
    }
 }
 
+void closeEye(int spd, int closeDistance) {
+     for (; posLid <= closeDistance; posLid += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+     eyelid.write(posLid);              // tell servo to go to position in variable 'pos'
+     delay(spd);                       // waits 15ms for the servo to reach the position
+   }
+}
+
 void openEye(int spd){
-  for (; posLid >= maxOpen; posLid -= 1) { // goes from 180 degrees to 0 degrees
+  for (; posLid >= maxOpen-5; posLid -= 1) { // goes from 180 degrees to 0 degrees
+     eyelid.write(posLid);              // tell servo to go to position in variable 'pos'
+     delay(spd);                       // waits 15ms for the servo to reach the position
+   }
+}
+
+void openEyeNormal(int spd){
+  if(posLid <= maxOpen) {
+    closeEye(5, 140);
+  }
+  for (; posLid >= 120; posLid -= 1) { // goes from 180 degrees to 0 degrees
      eyelid.write(posLid);              // tell servo to go to position in variable 'pos'
      delay(spd);                       // waits 15ms for the servo to reach the position
    }
 }
 
 void blinkEye(int spd) {
-    if(posLid < 90) {
-      closeEye(spd);
+    if(posLid < 150) {
+      shutEye(spd);
       openEye(spd);
     } else {
       openEye(spd);
